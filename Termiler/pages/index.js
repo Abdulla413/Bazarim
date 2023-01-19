024b8c6806b2b9351f03f45c98bb5d3e4d6623d8
abdulla <abdulla.arish@protonmail.com>
2023-01-19 07:44:34 +0100

    postcard component finished

---
+++ Termiler/pages/index.js
@@ -0,0 +1,13 @@
+import Banner from "../components/Banner"
+import Header from "../components/Header"
+import PostCard from "../components/PostCard"
+export default function Home() {
+ 
+  return (
+    <>
+    <Header/>
+    <Banner/>  
+    <PostCard/> 
+    </>
+  )
+}
