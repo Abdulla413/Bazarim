9aaf45711b7807b6b73dec53c0fb2fc55da70d44
abdulla <abdulla.arish@protonmail.com>
2023-01-23 14:16:44 +0100

    test

--- Termiler/pages/_app.js
+++ Termiler/pages/_app.js
@@ -1,8 +1,26 @@
 import '../styles/globals.css'
+import { useState, useEffect } from "react";
+import { getDocs, collection } from 'firebase/firestore';
+import db from '../db/firebase.config'
 
 function MyApp({ Component, pageProps }) {
+
+
+  useEffect(( )=>{
+    const getUsers = async ()=>{
+        const querySnapshot = await getDocs(collection(db, 'users'));
+        querySnapshot.docs.map(doc=>console.log(doc))
+        
+    }
+
+    getUsers()
+
+},[]);
+
   return <div dir='rtl'>
+ 
   <Component {...pageProps} />
+ 
 
   </div>
   
